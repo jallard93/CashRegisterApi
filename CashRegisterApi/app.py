43b94e7e5390c1d5f2f36205111c37f36eb0750a
Jonathan Allard <jonathanallard93@gmail.com>
2021-08-23 14:05:04 -0400

    Cleanup

--- CashRegisterApi/app.py
+++ CashRegisterApi/app.py
@@ -9,26 +9,24 @@ awslambda = boto3.client("lambda")
 @app.route('/cashregister/change', methods=['POST'], cors=True)
 def handler():
 
+    # read in the current request body and extract the billCounts
     request = app.current_request.json_body
-    
     request["billCounts"] = str(request["billCounts"])
-
     register_payload = json.dumps(request).encode()
 
+    # invoke the CashRegister-calculate_change function synchronously
     response = awslambda.invoke(
         FunctionName="CashRegister-calculate_change",
         InvocationType="RequestResponse",
         Payload=register_payload
     )
 
-    response = json.loads(response["Payload"].read())
-
-    change = response
-
+    # parse the response and return to requester
+    change_response = json.loads(response["Payload"].read())
     return Response(
-            body=change,
-            status_code=200
-        )
+        body=change_response,
+        status_code=200
+    )
 
 
 
